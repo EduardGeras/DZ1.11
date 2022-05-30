927501b316f31fc4bba5c9240d6cc9b89ec4c497
EduardGeras <103679703+EduardGeras@users.noreply.github.com>
2022-05-30 22:27:11 +0500

    Курсовая 1 повышенная сложность

--- src/Employee.java
+++ src/Employee.java
@@ -48,9 +48,7 @@ public class Employee {
 
     @Override
     public String toString() {
-
         return id + ". " + name + ", отдел " + department + ", зарплата " + salary + " рублей.";
     }
-
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     private static Employee[] list = new Employee[10];
     public static void main(String[] args) {
 
-
         list[0] = new Employee("Иванов Иван Иванович", 5, 76_000);
         list[1] = new Employee("Петров Пётр Петрович", 4, 98_000);
         list[2] = new Employee("Сидоров Сидор Сидорович", 3, 102_000);
@@ -20,9 +19,16 @@ public class Main {
         maxWage();
         averageSalary();
         staff();
+        index(10);
+        maxSalaryOfTheDepartment(2);
+        minSalaryOfTheDepartment(2);
+        departmentCosts(2);
+        averageSalaryInTheDepartment(2);
+        indexingTheDepartment(12, 2);
+        departmentList(2);
+        salaryLevel(95_000);
 
     }
-
 
     private static void lists() {
         for (int i = 0; i < list.length; i++) {
@@ -51,6 +57,7 @@ public class Main {
             }
         }
     }
+
     private static void maxWage() {
         double salary = list[0].getSalary();
         for (int i = 0; i < list.length - 1; i++) {
@@ -64,6 +71,7 @@ public class Main {
             }
         }
     }
+
     private static void averageSalary() {
         double summa = 0;
         for (int i = 0; i < list.length; i++) {
@@ -73,9 +81,94 @@ public class Main {
     }
 
     private static void staff() {
-
         for (int i = 0; i < list.length; i++) {
             System.out.println(list[i].getName());
+        }
+    }
+
+    private static void index (int a) {
+        for (int i = 0; i < list.length; i++) {
+            double newSalary = list[i].getSalary() / 100 * a + list[i].getSalary();
+            System.out.println("Проиндексировать зарплату на " + a + " процентов у сотрудника " + list[i].getName() + ". Новая зарплата равна " + newSalary);
+        }
+    }
+
+    private static void maxSalaryOfTheDepartment (int departament) {
+        double salary = list[0].getSalary();
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].getDepartment() == departament && salary < list[i].getSalary()) {
+                    salary = list[i].getSalary();
+            }
+        }
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].getDepartment() == departament) {
+                if (list[i].getSalary() >= salary) {
+                    System.out.println("Максимальную зарплату в отделе получает " + list[i].getName());
+                }
+            }
+        }
+    }
+    private static void minSalaryOfTheDepartment (int departament) {
+        double salary = list[0].getSalary();
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].getDepartment() == departament && salary > list[i].getSalary()) {
+                    salary = list[i].getSalary();
+            }
+        }
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].getDepartment() == departament) {
+                if (list[i].getSalary() <= salary) {
+                    System.out.println("Минимальную зарплату в отделе получает " + list[i].getName());
+                }
+            }
+        }
+    }
+
+    private static void departmentCosts (int departament) {
+        double summa = 0;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].getDepartment() == departament) {
+                summa = summa + list[i].getSalary();
+            }
+        }
+        System.out.println("Сумма затрат на зарплату по отделу равна " + summa + " рублей.");
+    }
+
+    private static void averageSalaryInTheDepartment (int departament) {
+        double summa = 0;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].getDepartment() == departament) {
+                summa = summa + list[i].getSalary();
+            }
+        }
+        System.out.println("Средняя зарплата отдела в месяц равна " + summa / 2 + " рублей.");
+    }
+
+    private static void indexingTheDepartment(double index, int departament) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].getDepartment() == departament) {
+                double newSalary = list[i].getSalary() / 100 * index + list[i].getSalary();
+                System.out.println("Проиндексировать зарплату на " + index + " процентов у сотрудника отдела " + list[i].getName() + ". Новая зарплата равна " + newSalary);
+            }
+        }
+    }
+
+    private static void departmentList(int departament) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].getDepartment() == departament) {
+                System.out.println("Сотрудник отдела " + list[i].getName() + " с зарплатой " + list[i].getSalary());
+            }
+        }
+    }
+
+    private static void salaryLevel(double a) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].getSalary() < a) {
+                System.out.println(list[i] + " Получает меньше " + a + " рублей.");
+            }
+            if (list[i].getSalary() >= a) {
+                System.out.println(list[i] + " Получает больше " + a + " рублей.");
+            }
         }
     }
 }
